8ff5862e9cd88c4a81c7705e8a9ffd290f717a78
Dương Thuần Bảo <thuanbao1509@gmail.com>
2021-07-30 22:19:44 +0700

    Release v2.0

--- landingpage/pages/index.js
+++ landingpage/pages/index.js
@@ -1,20 +1,17 @@
-import Head from 'next/head'
 import { ThemeProvider } from 'theme-ui';
 import theme from '../theme';
-
-import Image from 'next/image'
 
 import SEO from '../components/seo';
 import Layout from '../components/layout';
 import Banner from '../sections/banner';
 import KeyFeature from '../sections/key-feature';
-// import ServiceSection from '../sections/service-section';
-// import Feature from '../sections/feature';
-// import CoreFeature from '../sections/core-feature';
-// import WorkFlow from '../sections/workflow';
-// import Package from '../sections/package';
-// import TeamSection from '../sections/team-section';
-// import TestimonialCard from '../sections/testimonial';
+import ServiceSection from '../sections/service-section';
+import Feature from '../sections/feature';
+import CoreFeature from '../sections/core-feature';
+import WorkFlow from '../sections/workflow';
+import Package from '../sections/package';
+import TeamSection from '../sections/team-section';
+import TestimonialCard from '../sections/testimonial';
 
 export default function Home() {
   return (
@@ -23,13 +20,13 @@ export default function Home() {
           <SEO title="NextJS Landing Page" />
           <Banner />
           <KeyFeature />
-          {/* <ServiceSection />
+          <ServiceSection />
           <Feature />
           <CoreFeature />
           <WorkFlow />
           <Package />
           <TeamSection />
-          <TestimonialCard /> */}
+          <TestimonialCard />
         </Layout>
     </ThemeProvider>
   )
---
+++ landingpage/sections/team-section.js
@@ -0,0 +1,211 @@
+import { jsx as _jsx } from 'theme-ui';
+import { Container, Grid } from 'theme-ui';
+import SectionHeader from '../components/section-header';
+import TeamCard from '../components/team-card';
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+
+const data = [
+  {
+    id: 1,
+    imgSrc: '/assets/team/member-1.png',
+    altText: 'Saimon Harmer',
+    title: 'Saimon Harmer',
+    designation: 'CEO and Founder',
+    socialProfile: [
+      {
+        id: 1,
+        name: 'facebook',
+        path: '#',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 2,
+        name: 'twitter',
+        path: '#',
+        icon: <FaTwitter />,
+      },
+      {
+        id: 3,
+        name: 'instagram',
+        path: '#',
+        icon: <FaInstagram />,
+      },
+    ],
+  },
+  {
+    id: 2,
+    imgSrc: '/assets/team/member-2.png',
+    altText: 'Aaron Nunez',
+    title: 'Aaron Nunez',
+    designation: 'Founder',
+    socialProfile: [
+      {
+        id: 1,
+        name: 'facebook',
+        path: '#',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 2,
+        name: 'twitter',
+        path: '#',
+        icon: <FaTwitter />,
+      },
+      {
+        id: 3,
+        name: 'instagram',
+        path: '#',
+        icon: <FaInstagram />,
+      },
+    ],
+  },
+  {
+    id: 3,
+    imgSrc: '/assets/team/member-3.png',
+    altText: 'Aaron Nunez',
+    title: 'Aaron Nunez',
+    designation: 'Web Designer',
+    socialProfile: [
+      {
+        id: 1,
+        name: 'facebook',
+        path: '#',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 2,
+        name: 'twitter',
+        path: '#',
+        icon: <FaTwitter />,
+      },
+      {
+        id: 3,
+        name: 'instagram',
+        path: '#',
+        icon: <FaInstagram />,
+      },
+    ],
+  },
+  {
+    id: 4,
+    imgSrc: '/assets/team/member-4.png',
+    altText: 'Lina Jutila',
+    title: 'Lina Jutila',
+    designation: 'Web Developer',
+    socialProfile: [
+      {
+        id: 1,
+        name: 'facebook',
+        path: '#',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 2,
+        name: 'twitter',
+        path: '#',
+        icon: <FaTwitter />,
+      },
+      {
+        id: 3,
+        name: 'instagram',
+        path: '#',
+        icon: <FaInstagram />,
+      },
+    ],
+  },
+  {
+    id: 5,
+    imgSrc: '/assets/team/member-5.png',
+    altText: 'Saimon Harmer',
+    title: 'Saimon Harmer',
+    designation: 'CEO and Founder',
+    socialProfile: [
+      {
+        id: 1,
+        name: 'facebook',
+        path: '#',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 2,
+        name: 'twitter',
+        path: '#',
+        icon: <FaTwitter />,
+      },
+      {
+        id: 3,
+        name: 'instagram',
+        path: '#',
+        icon: <FaInstagram />,
+      },
+    ],
+  },
+  {
+    id: 6,
+    imgSrc: '/assets/team/member-6.png',
+    altText: 'Aaron Nunez',
+    title: 'Aaron Nunez',
+    designation: 'Web Designer',
+    socialProfile: [
+      {
+        id: 1,
+        name: 'facebook',
+        path: '#',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 2,
+        name: 'twitter',
+        path: '#',
+        icon: <FaTwitter />,
+      },
+      {
+        id: 3,
+        name: 'instagram',
+        path: '#',
+        icon: <FaInstagram />,
+      },
+    ],
+  },
+];
+
+export default function TeamSection() {
+  return (
+    <section>
+      <Container>
+        <SectionHeader
+          slogan="Out team"
+          title="The most qualified and talented individials"
+        />
+
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <TeamCard
+              key={item.id}
+              src={item.imgSrc}
+              altText={item.altText}
+              title={item.title}
+              designation={item.designation}
+              social={item.socialProfile}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  grid: {
+    mt: [0, null, -6, null, -4],
+    gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
+    gridTemplateColumns: [
+      'repeat(2,1fr)',
+      null,
+      'repeat(2,1fr)',
+      null,
+      'repeat(3,1fr)',
+    ],
+  },
+};
